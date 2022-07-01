e9e7c1e7f44c59f5732cb76d2edf920e06da74fc
yuhui liang <liangyuhui@matrixtime.com>
2022-07-01 18:19:34 +0800

    version1

---
+++ lib/1.py
@@ -0,0 +1,17 @@
+import numpy as np
+import cv2
+# src = cv2.imread('assets/1.jpg')
+# x = np.array([[[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5]],[[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5]]])
+# print(x)
+# print(x.shape)
+# print(type(x))
+# print(src.shape)
+
+# Xs, Ys = np.meshgrid(np.arange(6 * 2), np.arange(6 * 2))
+# print(Xs)
+# print(Ys)
+# print((Xs - 6) ** 2) 
+# print((Ys - 6) ** 2)
+
+x, y = [1,2]
+print(x, y)
